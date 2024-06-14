b949458bbc01604196c3cbbc9fd5d2923af6f9a6
Urielminpro705 <urielmata37@hotmail.com>
2024-06-13 22:04:28 -0600

    Empece a hacer la pagina de semestre

---
+++ TDMPW_411_3P_EX/Models/Semestre.cs
@@ -0,0 +1,8 @@
+﻿namespace TDMPW_411_3P_EX.Models;
+
+public class Semestre
+{
+    public string Nombre { get; set; }
+    public double[] ValorParciales { get; set; }
+    public double[] Calificaciones { get; set; }
+}
---
+++ TDMPW_411_3P_EX/ViewModels/SemestreViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TDMPW_411_3P_EX.Models;
+
+namespace TDMPW_411_3P_EX.ViewModels;
+
+public partial class SemestreViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private Semestre _semestre;
+
+    [ObservableProperty]
+    private string _nombre;
+
+    [ObservableProperty]
+    private string _val1;
+
+    [ObservableProperty]
+    private string _val2;
+
+    [ObservableProperty]
+    private string _val3;
+
+    [ObservableProperty]
+    private string _cal1;
+
+    [ObservableProperty]
+    private string _cal2;
+
+    public SemestreViewModel() {
+        Semestre = new Semestre {
+            Nombre = "Materia",
+            ValorParciales = [0,0,0],
+            Calificaciones = [0,0,0]
+        };
+    } 
+
+    [RelayCommand]
+    private void ActualizarDatos() {
+        Semestre.Nombre = Nombre;
+        Semestre.ValorParciales =
+        [
+            double.TryParse(Val1, out var val1) ? val1 : 0,
+            double.TryParse(Val2, out var val2) ? val2 : 0,
+            double.TryParse(Val3, out var val3) ? val3 : 0
+        ];
+        OnPropertyChanged(nameof(Semestre));
+    }
+}
